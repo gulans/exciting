0a9f369a46859864f9d4b9490bcec7ca753b3ab5
Janis Uzulis <janis.uzulis@lu.lv>
2024-03-26 15:23:51 +0200

    Created a new algorithm for going through the grid and finding the points that are inside MT.

--- src/src_lib/mod_rspace.f90
+++ src/src_lib/mod_rspace.f90
@@ -15,7 +15,7 @@ contains
 
 subroutine generate_rgrid_mt_data(lmax)
   use modinput
-  use mod_Gvector, only: ngrtot, ngrid, ngvec
+  use mod_Gvector, only: ngrtot, ngrid, ngvec, cfunir
   use mod_atoms, only: nspecies, natoms, atposc, idxas
   use mod_muffin_tin, only: rmt
   use constants, only: zzero
@@ -25,13 +25,14 @@ subroutine generate_rgrid_mt_data(lmax)
   real(8) :: a1(3),a2(3),a3(3),a1_abs,a2_abs,a3_abs
   real(8) :: base_mat(3,3),base_mat_inv(3,3),col(1,3)
   real(8) :: ratom(3),ratom_i(3), rv(3), rv_abs
-  integer :: is,ia,ias,ir1,ir2,ir3,i1,i2,i3,ig,lm
+  integer :: is,ia,ias,ir1,ir2,ir3,i1,i2,i3,ig,lm,igr
   real(8) :: tp(2)
   real(8) :: rgrid_mt_rv_tmp(3,natmtot,int(ngrtot/natmtot))
   integer :: rgrid_mt_map_tmp(int(ngrtot/natmtot),natmtot)
 
-
-
+integer :: p1,p2,p3, count_by_cfunir
+real(8) :: pvec(3), r_dist(3),rabs
+count_by_cfunir=0
   a1=input%structure%crystal%basevect(1, :)/ngrid(1)
   a2=input%structure%crystal%basevect(2, :)/ngrid(2)
   a3=input%structure%crystal%basevect(3, :)/ngrid(3)
@@ -46,6 +47,44 @@ subroutine generate_rgrid_mt_data(lmax)
   Call r3minv (base_mat, base_mat_inv)
   allocate(rgrid_nmtpoints(natmtot))
   rgrid_nmtpoints=0
+
+if (.true.) then
+do ir1=0,ngrid(1)-1
+  do ir2=0,ngrid(2)-1
+    do ir3=0,ngrid(3)-1
+      ig = ir3*ngrid(2)*ngrid(1) + ir2*ngrid(1) + ir1 + 1 
+      if (cfunir(ig).lt.0.8d0)then !inside of MT
+        count_by_cfunir=count_by_cfunir+1
+        rv = ir1*a1 + ir2*a2 + ir3*a3 !point coordinates
+        do is=1, nspecies
+          do ia=1, natoms(is)
+            ias=idxas(ia,is)
+            ratom=atposc (:, ia, is) 
+            do p1=-1,1
+              do p2=-1,1
+                do p3=-1,1
+                  pvec=input%structure%crystal%basevect(1, :)*p1 + input%structure%crystal%basevect(2, :)*p2 + input%structure%crystal%basevect(3, :)*p3
+                  r_dist = rv + pvec - ratom
+                  rabs=sqrt(r_dist(1)**2+r_dist(2)**2+r_dist(3)**2)
+                  if (rabs.le.rmt(is)) then !found which MT the point belongs to
+                    rgrid_nmtpoints(ias) = rgrid_nmtpoints(ias) + 1
+                    igr=rgrid_nmtpoints(ias)
+                    rgrid_mt_map_tmp(igr,ias)=ig
+                    rgrid_mt_rv_tmp(:,ias,igr)=r_dist
+                    !have to exit some loops to save time
+                  endif
+                enddo !p3
+              enddo !p2
+            enddo !p1
+
+          enddo!ia
+        enddo!is
+      endif ! cfunir
+    enddo
+  enddo
+enddo
+write(*,*)"count_by_cfunir",count_by_cfunir
+else 
   do is=1, nspecies
     do ia=1, natoms(is)
       ias=idxas(ia,is)
@@ -110,11 +149,9 @@ subroutine generate_rgrid_mt_data(lmax)
           enddo !ir1
         enddo !ir2
       enddo !ir3
-
-
-
     enddo !ia
   enddo !is
+endif
   rgrid_max_nmtpoints=maxval(rgrid_nmtpoints(:))
   
   allocate( rgrid_mt_rv(3,natmtot,rgrid_max_nmtpoints) )
@@ -160,6 +197,7 @@ subroutine generate_rgrid_mt_data(lmax)
   open(11,file='rgrid_info.dat',status='replace')
   write(11,*)"ngrtot",ngrtot
   write(11,*)"ngvec",ngvec
+  write(11,*)"total number of MT points:",sum(rgrid_nmtpoints)
   write(11,*)"rgrid_max_nmtpoints:",rgrid_max_nmtpoints
   write(11,*)"ias, rgrid_nmtpoints(ias)"
   do is=1, nspecies
@@ -178,7 +216,10 @@ subroutine generate_rgrid_mt_data(lmax)
       ratom=atposc (:, ia, is) 
       ias=idxas(ia,is)
       do ig=1, rgrid_nmtpoints(ias)
-        write(11,*)ias, rgrid_mt_rv(1,ias,ig)+ratom(1),rgrid_mt_rv(2,ias,ig)+ratom(2),rgrid_mt_rv(3,ias,ig)+ratom(3)
+        rv=rgrid_mt_rv(:,ias,ig)
+        rv_abs=dsqrt(rv(1)**2+rv(2)**2+rv(3)**2)
+        !write(11,*)ias, rgrid_mt_map(ig,ias),cfunir(rgrid_mt_map(ig,ias)),rgrid_mt_rv(1,ias,ig)+ratom(1),rgrid_mt_rv(2,ias,ig)+ratom(2),rgrid_mt_rv(3,ias,ig)+ratom(3)
+        write(11,*)ias, rgrid_mt_map(ig,ias),cfunir(rgrid_mt_map(ig,ias)),rv_abs,rgrid_mt_rv(1,ias,ig),rgrid_mt_rv(2,ias,ig),rgrid_mt_rv(3,ias,ig)
       enddo
     enddo
   enddo
@@ -194,7 +235,7 @@ subroutine generate_rgrid_mt_data(lmax)
   ! enddo
   ! close(11)
 
-
+!stop
 end subroutine 
 
 
