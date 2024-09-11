9a13cc9ec227f399c0b35302e922d5635e2fd804
Andris Gulans <gulans@physik.hu-berlin.de>
2024-09-11 15:18:14 +0300

    Minor optimizations in src/src_hybrids/poterfpw.f90

--- src/src_hybrids/poterfpw.f90
+++ src/src_hybrids/poterfpw.f90
@@ -32,6 +32,7 @@ subroutine poterfpw (ngvec1, rhoir,rhomt,igfft,sfacgq,ylmgq,gqc, jlgqsmallr,poti
 
       complex (8) :: zfmt1(nrmtmax),zfmt2(nrmtmax),zt1,zt2,zt3,zt4
       integer :: lmaxvr,is,ia,ias,ig,ifg,l,m,lm,ir,nr
+      complex (8), allocatable :: rhorcmt2(:,:)
 
       lmaxvr=input%groundstate%lmaxvr
 
@@ -40,26 +41,41 @@ potir(:) = cfunir(:)*rhoir(:)
 Call zfftifc (3, ngrid, -1, potir(:))  !to G space
 
 !!! Obtain Fourier coeficients of the density in the MT and add it in porir
+Allocate(rhorcmt2(lmmaxvr,nrmtmax))
 do is=1,nspecies
    nr=nrmt(is)
    do ia=1,natoms(is)
       ias=idxas(ia,is)
+      do ir=1,nr
+        do lm=1,lmaxvr
+          rhorcmt2(lm,ir)=rcmt(ir,is)**2 * rhomt(lm,ir,ias)
+        enddo
+!       zt1=4*pi*rcmt(ir,is)**2/omega
+!       do l=0,lmaxvr
+!         zt2=zt1/zil(l)
+!         do m=-l,l
+!           lm=idxlm(l,m)
+!           rhorcmt2(lm,ir)=zt1 * rhomt(lm,ir,ias)
+!         enddo
+!        enddo
+      enddo
+
       do ig=1,ngvec1
          ifg=igfft(ig)!(Gkqset%igkig(ig, 1, iq)) 
          do l=0,lmaxvr
             do m=-l,l 
                lm=idxlm(l,m)
-               zfmt1(:nr)=jlgqsmallr(:nr,l,ig,is)*rcmt(:nr,is)**2* rhomt(lm,:nr,ias)
+               zfmt1(:nr)=jlgqsmallr(:nr,l,ig,is)*rhorcmt2(lm,ir) !rcmt(:nr,is)**2* rhomt(lm,:nr,ias)
                call integ_cv (nr, is, zfmt1(:nr), zt3, mt_integw)
                zt4=zt3*4d0*pi*ylmgq(lm,ig)*conjg(sfacgq(ig, ias))/(omega*zil(l)) !!!Fāzes reizinātājs sfacgq(ig, ias) ??
+!              zt4=zt3*ylmgq(lm,ig)*conjg(sfacgq(ig, ias)) !!!Fāzes reizinātājs sfacgq(ig, ias) ??
                potir(ifg)=potir(ifg)+zt4
             enddo ! m
          enddo ! l
       enddo ! ig
    enddo ! ia 
 enddo ! is
-
-
+Deallocate(rhorcmt2)
 
 do ig=1, ngvec1
       ifg=igfft(ig)!(Gkqset%igkig(ig, 1, iq))   
@@ -87,15 +103,20 @@ do ig=1, ngvec1
          do ir=1,nr
             do ig=1, ngvec1
                ifg =igfft(ig)! igfft(Gkqset%igkig(ig, 1, iq))
+               zt1=potir(ifg)*sfacgq(ig, ias)
                do l=0,lmaxvr
-                  zt1=4d0*pi*potir(ifg)*zil(l)*jlgqsmallr(ir,l,ig,is) * sfacgq(ig, ias)
+                  zt2=zt1*jlgqsmallr(ir,l,ig,is) 
                   do m=-l,l                      
                      lm=idxlm(l,m)                     
-                     zt2=zt1*conjg(ylmgq(lm,ig))
-                     potmt(lm,ir,ias)=potmt(lm,ir,ias)+zt2
-                 
+                     potmt(lm,ir,ias)=potmt(lm,ir,ias)+zt2*conjg(ylmgq(lm,ig))
                   enddo 
                enddo
+            enddo
+            do l=0,lmaxvr
+              do m=-l,l
+                lm=idxlm(l,m)
+                potmt(lm,ir,ias)=4d0*pi*zil(l)*potmt(lm,ir,ias)
+              enddo
             enddo
          enddo
       enddo
