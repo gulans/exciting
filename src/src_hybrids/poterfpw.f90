0423288ed41168ce38cc740f5064910d4009ca04
Andris Gulans <gulans@physik.hu-berlin.de>
2024-09-18 14:16:36 +0300

    Bugfix and calls to dot products in poterfpw.f90

--- src/src_hybrids/poterfpw.f90
+++ src/src_hybrids/poterfpw.f90
@@ -32,9 +32,13 @@ subroutine poterfpw (ngvec1, rhoir,rhomt,igfft,sfacgq,ylmgq,gqc, jlgqsmallr,poti
 
       complex (8) :: zfmt1(nrmtmax),zfmt2(nrmtmax),zt1,zt2,zt3,zt4
       complex (8) :: zfun(nrmtmax,lmmaxvr)
-      integer :: lmaxvr,is,ia,ias,ig,ifg,l,m,lm,ir,nr
-      complex (8), allocatable :: rhorcmt2(:,:)
+      integer :: lmaxvr,is,ia,ias,ig,ifg,l,m,lm,ir,nr,sfld
+      complex (8), allocatable :: rhorcmt2(:,:),zfun2(:,:,:)
       real (8) :: fr(nrmtmax)
+      real (8) :: refr(nrmtmax,lmmaxvr),imfr(nrmtmax,lmmaxvr)
+      real (8) :: reint,imint
+      real (8), external :: ddot
+
 
       lmaxvr=input%groundstate%lmaxvr
 
@@ -46,24 +50,26 @@ Call zfftifc (3, ngrid, -1, potir(:))  !to G space
 Allocate(rhorcmt2(nrmtmax,lmmaxvr))
 do is=1,nspecies
    nr=nrmt(is)
+   do ir=1,nr
+     fr(ir)=mt_integw%intw(ir,is) * rcmt(ir,is)**2
+   enddo
    do ia=1,natoms(is)
       ias=idxas(ia,is)
-      do ir=1,nr
-        do lm=1,lmaxvr
-          rhorcmt2(ir,lm)=rcmt(ir,is)**2 * rhomt(lm,ir,ias)
+      do lm=1,lmmaxvr
+        do ir=1,nr
+          refr(ir,lm)=fr(ir) * dble(rhomt(lm,ir,ias))
+          imfr(ir,lm)=fr(ir) * dimag(rhomt(lm,ir,ias))
         enddo
       enddo
 
       do ig=1,ngvec1
          ifg=igfft(ig)!(Gkqset%igkig(ig, 1, iq)) 
          do l=0,lmaxvr
-            fr(1:nr)=mt_integw%intw(1:nr,is)*jlgqsmallr(1:nr,l,ig,is)
             do m=-l,l
                lm=idxlm(l,m)
-               zt3=0d0
-               do ir=1,nr
-                 zt3=zt3+rhorcmt2(ir,lm)*fr(ir)
-               enddo
+               reint=ddot(nr,refr(1,lm),1,jlgqsmallr(1,l,ig,is),1)
+               imint=ddot(nr,imfr(1,lm),1,jlgqsmallr(1,l,ig,is),1)
+               zt3=dcmplx(reint,imint)
                zt4=zt3*4d0*pi*ylmgq(lm,ig)*conjg(sfacgq(ig, ias))/(omega*zil(l)) !!!Fāzes reizinātājs sfacgq(ig, ias) ??
                potir(ifg)=potir(ifg)+zt4
             enddo ! m
@@ -101,8 +107,7 @@ do ig=1, ngvec1
           ifg =igfft(ig)! igfft(Gkqset%igkig(ig, 1, iq))
           zt1=potir(ifg)*sfacgq(ig, ias)
           do l=0,lmaxvr
-            do m=-l,l
-              lm=idxlm(l,m)
+            do lm=idxlm(l,-l),idxlm(l,l)
               zt2=zt1*conjg(ylmgq(lm,ig))
               do ir=1,nr
                 zfun(ir,lm)=zfun(ir,lm)+zt2*jlgqsmallr(ir,l,ig,is)
@@ -121,7 +126,6 @@ do ig=1, ngvec1
         enddo
       enddo
    enddo
-
    Call zfftifc (3, ngrid, 1, potir(:)) !to realspace 
 
 
